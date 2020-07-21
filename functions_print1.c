53b9c0284019e299f34c1674bc4df5b2ba39d7c7
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-21 11:14:26 -0500

    fix negatives

--- functions_print1.c
+++ functions_print1.c
@@ -1,55 +1,60 @@
 #include "holberton.h"
 /**
- * p_dec - prints
- * @list: argument
- * Return: number of characters
- */
-int p_dec(va_list list)
-{
-	int n = va_arg(list, int);
-	int num;
-	int last = n % 10;
-	int dig;
-	int exp = 1;
-	int i = 1;
-
-	n = n / 10;
-	num = n;
-	if (last < 0)
-	{
-		_putchar('-');
-		num = -num;
-		n = -n;
-		last = -last;
-		i++;
-	}
-	if (num > 0)
-	{
-		while (num / 10 != 0)
-		{
-			exp = exp * 10;
-			num = num / 10;
-		}
-		num = n;
-		while (exp > 0)
-		{
-			dig = num / exp;
-			_putchar(dig + '0');
-			num = num - (dig * exp);
-			exp = exp / 10;
-			i++;
-		}
-	}
-	_putchar(last + '0');
-	return (i);
-}
-/**
  * p_int - prints integer
  * @list: argument
  * Return: number of characters printed
  */
 int p_int(va_list list)
 {
+	int n = va_arg(list, int), num, last = n % 10, dig, exp = 1, i = 1;
+
+	n = n / 10;
+	num = n;
+
+	while (num / 10 != 0)
+	{
+		exp = exp * 10;
+		num = num / 10;
+	}
+	num = n;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		while (exp != 0)
+		{
+			dig = num / (-exp);
+			_putchar(dig + '0');
+			num = num % exp;
+			exp = exp / 10;
+			i++;
+		}
+		_putchar(-last + '0');
+	}
+	if (num > 0)
+	{
+		while (exp > 0)
+		{
+			dig = num / exp;
+			_putchar(dig + '0');
+			num = num % exp;
+			exp = exp / 10;
+			i++;
+		}
+		_putchar(last + '0');
+	}
+
+	return (i);
+}
+
+/**
+ * p_dec - prints
+ * @list: argument
+ * Return: number of characters
+ */
+int p_dec(va_list list)
+{
+
 	int n = va_arg(list, int);
 	int num;
 	int last = n % 10;
