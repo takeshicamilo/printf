75a33eda9d22df6179c99f891593d220f646ff0f
sebasvalencia726 <sebasvalencia726@gmail.com>
2020-07-21 11:24:59 -0500

    fix negatives

--- functions_print1.c
+++ functions_print1.c
@@ -10,40 +10,32 @@ int p_dec(va_list list)
 
 	n = n / 10;
 	num = n;
-
-	while (num / 10 != 0)
-	{
-		exp = exp * 10;
-		num = num / 10;
-	}
-	num = n;
-
-	if (num < 0)
+	if (last < 0)
 	{
 		_putchar('-');
-		while (exp != 0)
-		{
-			dig = num / (-exp);
-			_putchar(dig + '0');
-			num = num % exp;
-			exp = exp / 10;
-			i++;
-		}
-		_putchar(-last + '0');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
 	}
 	if (num > 0)
 	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
 		while (exp > 0)
 		{
 			dig = num / exp;
 			_putchar(dig + '0');
-			num = num % exp;
+			num = num - (dig * exp);
 			exp = exp / 10;
 			i++;
 		}
-		_putchar(last + '0');
 	}
-
+	_putchar(last + '0');
 	return (i);
 }
 /**
@@ -53,7 +45,6 @@ int p_dec(va_list list)
  */
 int p_int(va_list list)
 {
-
 	int n = va_arg(list, int);
 	int num;
 	int last = n % 10;
